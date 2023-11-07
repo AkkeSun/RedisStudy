630763733aa3ff9342bd9aa0090e97e0c818af10
od <od@sweettracker.co.kr>
2023-11-07 17:53:34 +0900

    perf: RedisUtil 개선

--- src/main/java/com/example/redisstudy/util/RedisUtil.java
+++ src/main/java/com/example/redisstudy/util/RedisUtil.java
@@ -1,23 +1,47 @@
 package com.example.redisstudy.util;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.Cursor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class RedisUtil {
 
-    private final RedisTemplate<String, Object> redisTemplate;
+    private RedisTemplate<String, Object> redisTemplate;
 
-    public void save (String key, Map<String, Object> map) {
-        redisTemplate.opsForHash().putAll(key, map);
+    @Autowired
+    public RedisUtil(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
     }
 
-    public Map<Object, Object> getData(String key) {
-        return redisTemplate.opsForHash().entries(key);
+    public void save (String key, String val) {
+        redisTemplate.opsForValue().set(key, val);
+    }
+
+    public Object getData(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    // keys::* 의 실제 Key 를 추출한다 (50개씩 끊어서 찾는다)
+    // redis keys() 함수로 key를 조회하면다른 redis 로직이 블로킹 된다.
+    // 이 때 cursor 를 대체하여 사용할 수 있다
+    public List<String> scanByPattern(String pattern) {
+        RedisConnection redisConnection = redisTemplate.getConnectionFactory().getConnection();
+        ScanOptions scanOptions = ScanOptions.scanOptions().count(50).match(pattern).build();
+        Cursor cursor = redisConnection.scan(scanOptions);
+
+        List<String> redisKeys = new ArrayList<>();
+        while (cursor.hasNext()) {
+            String key = new String((byte[]) cursor.next());
+            redisKeys.add(key);
+        }
+
+        return redisKeys;
     }
 
     public void deleteKeys(List<String> keys) {
