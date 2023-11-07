03bae850ac772e69ad27b475e8969bf4c87dcd25
od <od@sweettracker.co.kr>
2023-11-07 17:53:55 +0900

    test: 테스트코드 수정

--- src/test/java/com/example/redisstudy/RedisTest.java
+++ src/test/java/com/example/redisstudy/RedisTest.java
@@ -1,13 +1,21 @@
 package com.example.redisstudy;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.ExpectedCount.times;
+
 import com.example.redisstudy.domain.User;
+import com.example.redisstudy.service.CacheChecker;
 import com.example.redisstudy.service.UserService;
-import org.junit.jupiter.api.AfterEach;
+import com.example.redisstudy.util.RedisUtil;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 public class RedisTest {
@@ -15,10 +23,11 @@ public class RedisTest {
     @Autowired
     UserService userService;
 
-    @AfterEach
-    void reset(){
-        userService.deleteAll();
-    }
+    @MockBean
+    CacheChecker cacheChecker;
+
+    @Autowired
+    RedisUtil redisUtil;
 
     @Test
     @DisplayName("redis 데이터가 정상적으로 저장되고 조회되는지 테스트")
@@ -36,32 +45,13 @@ public class RedisTest {
         // then
         Assertions.assertEquals(user.getId(), savedUser.getId());
         Assertions.assertEquals(user.getName(), savedUser.getName());
-    }
-
-    @Test
-    @DisplayName("redis 데이터가 정상적으로 삭제되는지 테스트")
-    void deleteTest() {
-
-        // given
-        User user = User.builder()
-            .id(99)
-            .name("test")
-            .build();
-
-        // when
-        userService.save(user);
-        userService.delete(99);
-        User savedUser = userService.getUser(99);
-
-        // then
-        Assertions.assertNull(savedUser);
+        userService.delete(user.getId());
     }
 
     @Test
     @DisplayName("redis 캐시가 정상적으로 처리되는지 테스트")
     void cacheTest() {
 
-        // given
         User user = User.builder()
             .id(100)
             .name("sun")
@@ -72,9 +62,44 @@ public class RedisTest {
         User savedUser = userService.getUser(user.getId());
         User cacheData = userService.getUser(user.getId());
 
+        verify(cacheChecker, Mockito.times(1)).cacheCheck();
         Assertions.assertEquals(cacheData.getId(), savedUser.getId());
         Assertions.assertEquals(cacheData.getName(), savedUser.getName());
+        userService.delete(user.getId());
     }
 
+    @Test
+    @DisplayName("redis 데이터가 정상적으로 삭제되는지 테스트")
+    void deleteTest() {
 
+        // given
+        User user = User.builder()
+            .id(10)
+            .name("test")
+            .build();
+        User user2 = User.builder()
+            .id(11)
+            .name("test")
+            .build();
+        User user3 = User.builder()
+            .id(12)
+            .name("test")
+            .build();
+        userService.save(user);
+        userService.save(user2);
+        userService.save(user3);
+
+        // when
+        List<String> keys = redisUtil.scanByPattern("key::*");
+        redisUtil.deleteKeys(keys);
+
+        // then
+        User savedUser = userService.getUser(user.getId());
+        User savedUser2 = userService.getUser(user2.getId());
+        User savedUser3 = userService.getUser(user3.getId());
+        Assertions.assertEquals(keys.size(), 3);
+        Assertions.assertNull(savedUser);
+        Assertions.assertNull(savedUser2);
+        Assertions.assertNull(savedUser3);
+    }
 }
